0c4a5bfbf9f43c462809fefe725909ff451e7af5
KJacks <94314144+KyriakosTsartsarakoss@users.noreply.github.com>
2024-09-29 18:10:45 +0300

    Add files via upload

---
+++ service-user-data/server.js
@@ -0,0 +1,105 @@
+const { Kafka } = require("kafkajs")
+const mongoose = require("mongoose")
+
+const kafka = new Kafka({
+    clientId: "service-user-data",
+    brokers: ["kafka-broker:9092"],
+    retries: 10,
+});
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: "service-user-data" });
+
+
+const main = async () => {
+
+    await mongoose.connect("mongodb://user:pass@mongodb-user-data:27017/Users?authSource=admin");
+
+    const userSchema = new mongoose.Schema({
+        email: String,
+        name: String,
+        credits: Number
+    })
+
+    const User = mongoose.model("User", userSchema)
+
+    await producer.connect()
+    await consumer.connect()
+
+    await consumer.subscribe({
+        topics: [
+            "user-create-request",
+            "user-credits-update-request",
+            "user-get-request",
+            "execution-request"
+        ],
+        fromBeginning: false
+    })
+
+    await consumer.run({
+        eachMessage: async ({ message, topic }) => {
+
+            if (topic === "user-create-request") {    
+                const email = message.key.toString();
+                const name = message.value.toString();
+
+                const newUser = new User({
+                    email: email,
+                    name: name,
+                    credits: 0
+                });
+                
+                await newUser.save();
+            }
+
+            else if (topic === "user-credits-update-request") {
+                const email = message.key.toString();
+                const added_credits = Number(message.value.toString());
+
+                await User.findOneAndUpdate(
+                    { email: email},
+                    { $inc: { credits: added_credits }}
+                );
+            }
+
+            else if (topic == "user-get-request") {
+                const email = message.value.toString();
+                const user = await User.findOne({ email: email });
+
+                producer.send({
+                    topic: "user-get-response",
+                    messages: [
+                        { key: email, value: JSON.stringify(user) }
+                    ]
+                });
+            }
+
+            else if (topic == "execution-request") {
+                const email = message.key.toString();
+                const submission_name = message.value.toString()
+                const user = await User.findOne({ email: email });
+                if (user.credits > 0) {
+                    producer.send({
+                        topic: `execution-request-accepted`,
+                        messages: [
+                            {
+                                key: email,
+                                value: submission_name
+                            }
+                        ]
+                    })
+                    await User.findOneAndUpdate(
+                        { email: email},
+                        { $inc: { credits: -1 }}
+                    );
+                }
+                
+            }
+        }
+    });
+    console.log("Service user-data is running");
+
+};
+
+
+main();
